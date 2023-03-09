747a55cf7d9b1f9b0dece7e1a515007cd7188a85
Canna Wen <cannawen@gmail.com>
2023-03-09 14:48:19 -0500

    Add README and Dota 2 Game State Integration library

---
+++ README.md
@@ -0,0 +1,7 @@
+## Setup
+### Enable GSI
+Steam -> Right click Dota 2 -> Properties
+ - General -> Launch Options -> Add `-gamestateintegration` to launch options
+ - Local Files -> Browse... mkdir and add config file to `game\dota\cfg\gamestate_integration\`
+### Start application
+`npm start`
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const d2gsi = require('dota2-gsi');
+
+const server = new d2gsi();
+
+server.events.on('newclient', function(client) {
+    console.log("New client connection, IP address: " + client.ip);
+    if (client.auth && client.auth.token) {
+        console.log("Auth token: " + client.auth.token);
+    } else {
+        console.log("No Auth token");
+    }
+  });
