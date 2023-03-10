b8c527047ae864d6a8577adce2135422182ff13c
Canna Wen <cannawen@gmail.com>
2023-03-10 11:25:42 -0500

    Revert previous change to make a single audio player again Keep a single subscription object that holds onto player and connection [experiment]

--- announce.js
+++ announce.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const AudioFiles = require('./AudioFiles')
 const Discord = require('discord.js');
-const { joinVoiceChannel, VoiceConnectionStatus, createAudioPlayer, createAudioResource, AudioPlayer, AudioPlayerStatus } = require('@discordjs/voice');
+const { joinVoiceChannel, VoiceConnectionStatus, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
 
 const discordClient = new Discord.Client({ intents: [131071] });
 
-var connection = null;
+var subscription = null;
 
 const HARD_CODED_GUILD_NAME = "Best Dota";
 const HARD_CODED_VOICE_CHANNEL_NAME = "Dota 2";
@@ -16,11 +16,22 @@ discordClient.on('ready', () => {
     const guild = Array.from(discordClient.guilds.cache.values()).find((guild) => guild.name == HARD_CODED_GUILD_NAME);
     const channel = Array.from(guild.channels.cache.values()).find((channel => channel.name == HARD_CODED_VOICE_CHANNEL_NAME));
 
-    connection = joinVoiceChannel({
+    const connection = joinVoiceChannel({
         channelId: channel.id,
         guildId: channel.guild.id,
         adapterCreator: channel.guild.voiceAdapterCreator,
     });
+
+    const player = createAudioPlayer();
+    player.on('stateChange', (oldState, newState) => {
+        if (oldState.status == newState.status) {
+            console.log(`Audio player state ${oldState.status}`);
+        } else {
+            console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
+        }
+    });
+
+    subscription = connection.subscribe(player);
 
     connection.on(VoiceConnectionStatus.Ready, () => {
         console.log('------- Ready to play audio!');
@@ -40,24 +51,6 @@ discordClient.login(process.env.DISCORD_CLIENT_TOKEN);
 module.exports = (constant) => {
     if (constant) {
         console.log("Playing " + AudioFiles[constant])
-        const player = createAudioPlayer();
-        player.play(createAudioResource(AudioFiles[constant]));
-        player.on(AudioPlayerStatus.Idle, () => {
-            console.log("Deallocating audio player")
-            player.stop();
-        })
-        player.on(AudioPlayerStatus.AutoPaused, () => {
-            console.log("Deallocating audio player")
-            player.stop();
-        })
-        player.on('stateChange', (oldState, newState) => {
-            if (oldState.status == newState.status) {
-                console.log(`Audio player state ${oldState.status}`);
-            } else {
-                console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
-            }
-        });
-
-        connection.subscribe(player);
+        subscription.player.play(createAudioResource(AudioFiles[constant]));
     }
 }
