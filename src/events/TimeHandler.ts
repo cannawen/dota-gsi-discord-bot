81da4501f6ec445ace89b40a1b181bd72127f29e
Canna Wen <cannawen@gmail.com>
2023-03-12 19:24:54 -0400

    Create handler for GSI clock time events

---
+++ src/events/TimeHandler.ts
@@ -0,0 +1,5 @@
+import runes from "./runes/runes";
+
+export default function handle(time: number) {
+    runes(time).execute();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const d2gsi = require("dota2-gsi");
 import gsi from "./gsi";
+import log = require("npmlog");
 
-gsi();
+const server = new d2gsi();
+server.events.on("newclient", (client: { ip: any; auth: any; on: any; }) => {
+    log.info("Dota 2 GSI", "New client connection, IP address:", client.ip);
+    if (client.auth && client.auth.token) {
+        log.info("Dota 2 GSI", "Auth token:", client.auth.token);
+    } else {
+        log.info("Dota 2 GSI", "No auth token");
+    }
+    gsi(client);
+});
