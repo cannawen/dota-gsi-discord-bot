b8dd93e807b556e08c8c94efae6a6cbea5fae930
Canna Wen <cannawen@gmail.com>
2023-03-13 00:06:44 -0400

    Do not warn about bounty runes before game starts [closes #26]

--- src/events/runes/index.ts
+++ src/events/runes/index.ts
@@ -3,6 +3,10 @@ import SideEffect from "../SideEffect";
 import timeToRuneIdBitmap from "./logic";
 
 export default function handler(time: number) {
+    if (time <= Constants.Time.GAME_START_TIME) {
+        return new SideEffect.None();
+    }
+
     const audioKey = timeToRuneIdBitmap(time + Constants.Time.ADVANCED_WARNING_TIME_BEFORE_RUNE_SPAWN);
 
     const audioFileName = Constants.Audio[audioKey];
