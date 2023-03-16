3edd84ee563427e53adfe2ce39e8f358d509db36
Canna Wen <cannawen@gmail.com>
2023-03-16 01:16:06 -0400

    Refactor how loggers construct from default settings

--- src/log.ts
+++ src/log.ts
@@ -1,71 +1,5 @@
 import winston from "winston";
 
-const baseLoggerConfig = {
-    format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.splat(),
-        winston.format.simple(),
-        winston.format.json(),
-    ),
-    level:      "info",
-    transports: [
-        new winston.transports.File({
-            filename: "error.log",
-            level:    "error",
-        }),
-        new winston.transports.File({
-            filename: "combined.log",
-        }),
-        new winston.transports.Console({
-            format: winston.format.simple(),
-        }),
-    ],
-};
-
-const baseLoggerConfig2 = {
-    format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.splat(),
-        winston.format.simple(),
-        winston.format.json(),
-    ),
-    level:      "info",
-    transports: [
-        new winston.transports.File({
-            filename: "error.log",
-            level:    "error",
-        }),
-        new winston.transports.File({
-            filename: "combined.log",
-        }),
-        new winston.transports.Console({
-            format: winston.format.simple(),
-        }),
-    ],
-};
-
-const baseLoggerConfig3 = {
-    format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.splat(),
-        winston.format.simple(),
-        winston.format.json(),
-    ),
-    level:      "info",
-    transports: [
-        new winston.transports.File({
-            filename: "error.log",
-            level:    "error",
-        }),
-        new winston.transports.File({
-            filename: "combined.log",
-        }),
-        new winston.transports.Console({
-            format: winston.format.simple(),
-        }),
-    ],
-};
-
 // Available levels
 // {
 //     error: 0,
@@ -77,35 +11,67 @@ const baseLoggerConfig3 = {
 //     silly: 6
 // }
 
-const log = winston.createLogger(baseLoggerConfig);
-log.format = winston.format.combine(
-    log.format,
-    winston.format.label({
-        label:   "GENERAL",
-        message: true,
-    }),
-);
-log.level = "info";
+const LOG_LEVEL = "info";
+const DISCORD_LOG_LEVEL = "info";
+const GSI_LOG_LEVEL = "info";
 
-const discordLog = winston.createLogger(baseLoggerConfig2);
-discordLog.format = winston.format.combine(
-    discordLog.format,
-    winston.format.label({
-        label:   "DISCORD",
-        message: true,
-    }),
-);
-discordLog.level = "error";
+const defaultFormatArray = [
+    winston.format.colorize(),
+    winston.format.splat(),
+    winston.format.simple(),
+    winston.format.json(),
+];
 
-const gsiLog = winston.createLogger(baseLoggerConfig3);
-gsiLog.format = winston.format.combine(
-    gsiLog.format,
-    winston.format.label({
-        label:   "GSI   ",
-        message: true,
-    }),
-);
-gsiLog.level = "error";
+function createTransports() {
+    return [
+        new winston.transports.File({
+            filename: "error.log",
+            level:    "error",
+        }),
+        new winston.transports.File({
+            filename: "combined.log",
+        }),
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        }),
+    ];
+}
+
+const log = winston.createLogger({
+    format: winston.format.combine(
+        ...defaultFormatArray,
+        winston.format.label({
+            label:   "GENERAL",
+            message: true,
+        })
+    ),
+    level:      LOG_LEVEL,
+    transports: createTransports(),
+});
+
+const discordLog = winston.createLogger({
+    format: winston.format.combine(
+        ...defaultFormatArray,
+        winston.format.label({
+            label:   "DISCORD",
+            message: true,
+        })
+    ),
+    level:      DISCORD_LOG_LEVEL,
+    transports: createTransports(),
+});
+
+const gsiLog = winston.createLogger({
+    format: winston.format.combine(
+        ...defaultFormatArray,
+        winston.format.label({
+            label:   "GSI    ",
+            message: true,
+        })
+    ),
+    level:      GSI_LOG_LEVEL,
+    transports: createTransports(),
+});
 
 export default log;
 export {
