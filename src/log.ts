5d834d0802fd0ebb060770e1313e02376bc5a80b
Canna Wen <cannawen@gmail.com>
2023-03-18 00:36:00 -0400

    Change GSI Item flag back

--- src/log.ts
+++ src/log.ts
@@ -91,7 +91,7 @@ const discord = winston.createLogger(createMap("[DISCORD]".blue, DISCORD_LOG_LEV
 const gsi = winston.createLogger(createMap("[GSI]".magenta, GSI_LOG_LEVEL_DEBUG));
 const gsiEvents = winston.createLogger(createMap("[GSI EVENTS]".magenta, GSI_LOG_LEVEL_DEBUG));
 const gsiGameState = winston.createLogger(createMap("[GSI GAME STATE]".magenta, GSI_LOG_LEVEL_DEBUG));
-const gsiItems = winston.createLogger(createMap("[GSI ITEMS]".magenta, "silly"));
+const gsiItems = winston.createLogger(createMap("[GSI ITEMS]".magenta, GSI_LOG_LEVEL_DEBUG));
 const gsiTime = winston.createLogger(createMap("[GSI TIME]".magenta, GSI_LOG_LEVEL_DEBUG));
 
 export default {
