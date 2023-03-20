4ea07e570fd282b777b961268459a632d103ad77
Canna Wen <cannawen@gmail.com>
2023-03-20 09:59:37 -0400

    Refactor GSI Items to have observer and live game handlers separately

--- src/gsi/GsiItems.ts
+++ src/gsi/GsiItems.ts
@@ -9,21 +9,24 @@ import Topic from "../Topic";
 class GsiItems {
     private handleItems(itemContainer: IItemContainer) {
         log.debug("gsi", "%s", itemContainer);
-        const items = PlayerItems.create(itemContainer);
-        if (items) {
-            return items;
+        return PlayerItems.create(itemContainer);
+    }
+
+    public handleState(state: IDota2State): PlayerItems | void {
+        if (state.items) {
+            return this.handleItems(state.items);
         }
     }
 
-    public handleState(state: IDota2State | IDota2ObserverState): PlayerItems | void {
-        if (Array.isArray(state.items)) { // If we are in observer mode
-            return this.handleItems(state.items[0]); // Pick the first person
-        } else if (state.items) {
-            return this.handleItems(state.items);
+    public handleStateObserver(state: IDota2ObserverState): PlayerItems | void {
+        // If we are in observer mode
+        // Pick the first person
+        if (state.items) {
+            return this.handleItems(state.items[0]);
         }
     }
 }
 
 const component = new GsiItems();
 broker.register("GSI/ITEMS", Topic.GSI_DATA_LIVE, Topic.DOTA_2_ITEMS, component.handleState.bind(component));
-broker.register("GSI/ITEMS", Topic.GSI_DATA_OBSERVER, Topic.DOTA_2_ITEMS, component.handleState.bind(component));
+broker.register("GSI/ITEMS", Topic.GSI_DATA_OBSERVER, Topic.DOTA_2_ITEMS, component.handleStateObserver.bind(component));
