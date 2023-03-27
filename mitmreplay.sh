f680bcb5838ed48d9c129e57fa47d9ae946f83c3
Canna Wen <cannawen@gmail.com>
2023-03-27 09:48:06 -0400

    Redirect mitmproxy from local to remote endpoint
    Co-authored-by: Rafal Dittwald <rafal.dittwald@gmail.com>

---
+++ mitmreplay.sh
@@ -0,0 +1,2 @@
+mitmdump -M "|~all|http://localhost:9001/gsi|http://dota-coach.fly.dev/gsi" -nC mitmproxy-flows/water-rune_canna
+mitmdump -M "|~all|http://localhost:9001/gsi|http://dota-coach.fly.dev/gsi" -nC mitmproxy-flows/water-rune_wrath
