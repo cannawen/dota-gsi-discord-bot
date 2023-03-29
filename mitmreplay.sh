756596c99329a9a92672f78486cb7580baac3069
Canna Wen <cannawen@gmail.com>
2023-03-29 12:01:20 -0400

    Move remote mitmproxy flows to npm scripts

--- mitmreplay.sh
+++
@@ -1,2 +0,0 @@
-mitmdump -M "|~all|http://localhost:9001/gsi|http://dota-coach.fly.dev/gsi" -nC mitmproxy-flows/water-rune_canna
-mitmdump -M "|~all|http://localhost:9001/gsi|http://dota-coach.fly.dev/gsi" -nC mitmproxy-flows/water-rune_wrath
