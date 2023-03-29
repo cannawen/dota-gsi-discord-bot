75658e9c0b007c5fbecfd71d766feeadde3b127c
Canna Wen <cannawen@gmail.com>
2023-03-28 21:54:42 -0400

    Add student page for sound events relevant to a single user [#43]
    Co-authored-by: Rafal Dittwald <rafal.dittwald@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,32 @@
 import express from "express";
 import gsiParser from "./gsiParser";
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 
+app.use(
+    "/resources/audio",
+    express.static(path.join(__dirname, "../resources/audio"))
+);
+
 app.get("/", (req, res) => {
     res.status(200).send("hello :3");
 });
 
-app.get("/coach/:studentId", (req, res) => {
-    res.status(200).send(`html+js for ${req.params.studentId}`);
+app.get("/coach/:studentId/", (req, res) => {
+    res.status(200).sendFile(
+        path.join(__dirname, "../resources/studentPage.html")
+    );
 });
 
 app.get("/coach/:studentId/poll", (req, res) => {
-    res.status(200).send(`Playing audio for student ${req.params.studentId}`);
+    const id = req.params.studentId;
+
+    res.status(200).json([
+        Math.random() > 0.85 ? "resources/audio/rune-sound.mp3" : null,
+    ]);
 });
 
 app.head("/gsi", (req, res) => {
