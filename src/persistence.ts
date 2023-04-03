fd3a4cb961fe6acf288780bbe99ceda40be27b69
Canna Wen <cannawen@gmail.com>
2023-04-03 09:55:05 -0400

    Add ability to read and write from redis [#45]

---
+++ src/persistence.ts
@@ -0,0 +1,35 @@
+import redis from "redis";
+
+const redisUrl = process.env.REDIS_URL;
+
+async function persistData() {
+    if (!redisUrl) return;
+
+    const client = redis.createClient({
+        url: redisUrl,
+    });
+    await client.connect();
+
+    await client.set("mykey", "Hello from node redis", { EX: 120 });
+
+    await client.quit();
+}
+
+async function readData() {
+    if (!redisUrl) return;
+
+    const client = redis.createClient({
+        url: redisUrl,
+    });
+    await client.connect();
+
+    const myKeyValue = await client.get("mykey");
+    console.log(myKeyValue);
+
+    await client.quit();
+}
+
+export default {
+    persistData,
+    readData,
+};
