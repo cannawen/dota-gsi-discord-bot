f1175de5571896e27be3e0914ccada68f7e3230a
Canna Wen <cannawen@gmail.com>
2023-04-04 10:26:06 -0400

    Add try-catch around persistence in case we have not started the redis server [#45]

--- src/persistence.ts
+++ src/persistence.ts
@@ -1,3 +1,4 @@
+/* eslint-disable sort-keys */
 import { createClient } from "redis";
 import dotenv = require("dotenv");
 import log from "./log";
@@ -6,41 +7,47 @@ dotenv.config();
 const redisUrl = process.env.REDIS_URL;
 
 async function saveData(allData: string) {
-    log.info("app", "Saving data in redis %s", allData.magenta);
-    const client = createClient({
-        url: redisUrl,
-        socket: {
-            reconnectStrategy: false,
-        },
-    });
-    client.on("error", (err) => console.log("Redis Client Error", err));
+    try {
+        log.info("app", "Saving data in redis %s", allData.magenta);
+        const client = createClient({
+            url: redisUrl,
+            socket: {
+                reconnectStrategy: false,
+            },
+        });
 
-    await client.connect();
+        await client.connect();
 
-    await client.set("restore", allData, { EX: 3600 });
+        await client.set("restore", allData, { EX: 3600 });
 
-    await client.quit();
+        await client.quit();
+    } catch (error) {
+        log.warn("app", "Unable to save data to redis, %s", error);
+    }
 }
 
 async function readData() {
-    const client = createClient({
-        url: redisUrl,
-        socket: {
-            reconnectStrategy: false,
-        },
-    });
-    client.on("error", (err) => console.log("Redis Client Error", err));
+    try {
+        const client = createClient({
+            url: redisUrl,
+            socket: {
+                reconnectStrategy: false,
+            },
+        });
 
-    await client.connect();
+        await client.connect();
 
-    const myKeyValue = await client.get("restore").then((data) => {
-        log.info("app", "Reading data from redis %s", data?.magenta);
-        return data;
-    });
+        const dataPromise = await client.get("restore").then((data) => {
+            log.info("app", "Reading data from redis %s", data?.magenta);
+            return data;
+        });
 
-    client.quit();
+        client.quit();
 
-    return myKeyValue;
+        return dataPromise;
+    } catch (error) {
+        log.warn("app", "Unable to read data from redis, %s", error);
+    }
 }
 
 export default {
