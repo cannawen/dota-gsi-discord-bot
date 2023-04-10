15f6b405225b3aebae7c70285991a0f2b2682ff6
Canna Wen <cannawen@gmail.com>
2023-04-09 23:14:13 -0400

    Add try-catch around persistence (in case we changed topic labels across restarts) [#48]
    Co-authored-by: Rafal Dittwald <rafal.dittwald@gmail.com>

--- src/persistence.ts
+++ src/persistence.ts
@@ -17,35 +17,51 @@ if (!fs.existsSync(STUDENT_DATA_DIRECTORY_PATH)) {
 }
 
 function saveRestartData(data: string) {
-    log.info("app", "Saving data %s", data);
-    fs.writeFileSync(RESTART_DATA_FILE_PATH, data);
-}
-
-function readRestartData() {
-    if (fs.existsSync(RESTART_DATA_FILE_PATH)) {
-        const data = fs.readFileSync(RESTART_DATA_FILE_PATH, "utf8");
-        fs.unlinkSync(RESTART_DATA_FILE_PATH);
-        log.info("app", "Reading data %s", data);
-        return data;
+    try {
+        log.info("app", "Saving data %s", data);
+        fs.writeFileSync(RESTART_DATA_FILE_PATH, data);
+    } catch (error) {
+        log.error("app", "Unable to write restart data %e", error);
     }
 }
 
-function readStudentData(studentId: string) {
-    const studentFile = path.join(
-        STUDENT_DATA_DIRECTORY_PATH,
-        `${studentId}.json`
-    );
-    if (fs.existsSync(studentFile)) {
-        return fs.readFileSync(studentFile, "utf8");
+function readRestartData() {
+    try {
+        if (fs.existsSync(RESTART_DATA_FILE_PATH)) {
+            const data = fs.readFileSync(RESTART_DATA_FILE_PATH, "utf8");
+            fs.unlinkSync(RESTART_DATA_FILE_PATH);
+            log.info("app", "Reading data %s", data);
+            return data;
+        }
+    } catch (error) {
+        log.error("app", "Unable to read restart data %e", error);
     }
 }
 
 function saveStudentData(studentId: string, data: string) {
-    const studentFile = path.join(
-        STUDENT_DATA_DIRECTORY_PATH,
-        `${studentId}.json`
-    );
-    fs.writeFileSync(studentFile, data);
+    try {
+        const studentFile = path.join(
+            STUDENT_DATA_DIRECTORY_PATH,
+            `${studentId}.json`
+        );
+        fs.writeFileSync(studentFile, data);
+    } catch (error) {
+        log.error("app", "Unable to write student data %e", error);
+    }
+}
+
+function readStudentData(studentId: string) {
+    try {
+        const studentFile = path.join(
+            STUDENT_DATA_DIRECTORY_PATH,
+            `${studentId}.json`
+        );
+        if (fs.existsSync(studentFile)) {
+            return fs.readFileSync(studentFile, "utf8");
+        }
+    } catch (error) {
+        log.error("app", "Unable to read student data %e", error);
+    }
 }
 
 export default {
