62a8d19879129f5b134378be1fc583d174e03bfc
Canna Wen <cannawen@gmail.com>
2023-04-10 23:32:14 -0400

    Add tests for RuleConfigurable

---
+++ src/engine/__tests__/RuleConfigurable.test.ts
@@ -0,0 +1,40 @@
+import EffectConfig from "../../EffectConfig";
+import Fact from "../Fact";
+import { getResults } from "../../__tests__/helpers";
+import RuleConfigurable from "../RuleConfigurable";
+import Topic from "../Topic";
+
+describe("RuleConfigurable", () => {
+    let configTopic: Topic<EffectConfig>;
+    let topic: Topic<number>;
+    let rule: RuleConfigurable;
+
+    beforeEach(() => {
+        configTopic = new Topic<EffectConfig>("configTopic");
+        topic = new Topic<number>("topic");
+        rule = new RuleConfigurable(
+            "test",
+            configTopic,
+            [topic],
+            (get, effect) => new Fact(effect, "foo.mp3")
+        );
+    });
+    test("PRIVATE", () => {
+        const results = getResults(rule, { configTopic: EffectConfig.PRIVATE });
+        expect(results).toContainFact("playPrivateAudioFile", "foo.mp3");
+    });
+    test("PUBLIC", () => {
+        const results = getResults(rule, { configTopic: EffectConfig.PUBLIC });
+        expect(results).toContainFact("playPublicAudioFile", "foo.mp3");
+    });
+    test("PUBLIC_INTERRUPTING", () => {
+        const results = getResults(rule, {
+            configTopic: EffectConfig.PUBLIC_INTERRUPTING,
+        });
+        expect(results).toContainFact("playInterruptingAudioFile", "foo.mp3");
+    });
+    test("NONE", () => {
+        const results = getResults(rule, { configTopic: EffectConfig.NONE });
+        expect(results).toBeUndefined();
+    });
+});
