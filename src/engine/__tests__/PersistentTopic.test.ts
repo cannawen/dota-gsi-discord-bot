62b10854f9e3a6ff3bd01e835b43718efa0a44fa
Canna Wen <cannawen@gmail.com>
2023-04-11 20:44:46 -0400

    Use .toBe(true/false) instead of .toBeTruthy/Falsy()

--- src/engine/__tests__/PersistentTopic.test.ts
+++ src/engine/__tests__/PersistentTopic.test.ts
@@ -3,20 +3,20 @@ import PersistentTopic from "../PersistentTopic";
 describe("PersistentTopic", () => {
     test("persistAcrossRestarts", () => {
         const topic = new PersistentTopic("a", { persistAcrossRestarts: true });
-        expect(topic.persistAcrossRestarts).toBeTruthy();
-        expect(topic.persistAcrossGames).toBeFalsy();
-        expect(topic.persistForever).toBeFalsy();
+        expect(topic.persistAcrossRestarts).toBe(true);
+        expect(topic.persistAcrossGames).toBe(false);
+        expect(topic.persistForever).toBe(false);
     });
     test("persistAcrossGames", () => {
         const topic = new PersistentTopic("a", { persistAcrossGames: true });
-        expect(topic.persistAcrossRestarts).toBeFalsy();
-        expect(topic.persistAcrossGames).toBeTruthy();
-        expect(topic.persistForever).toBeFalsy();
+        expect(topic.persistAcrossRestarts).toBe(false);
+        expect(topic.persistAcrossGames).toBe(true);
+        expect(topic.persistForever).toBe(false);
     });
     test("persistForever", () => {
         const topic = new PersistentTopic("a", { persistForever: true });
-        expect(topic.persistAcrossRestarts).toBeTruthy();
-        expect(topic.persistAcrossGames).toBeTruthy();
-        expect(topic.persistForever).toBeTruthy();
+        expect(topic.persistAcrossRestarts).toBe(true);
+        expect(topic.persistAcrossGames).toBe(true);
+        expect(topic.persistForever).toBe(true);
     });
 });
