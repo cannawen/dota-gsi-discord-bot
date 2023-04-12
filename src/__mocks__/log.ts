01a7d02f717ae288c24801fb0d3c6c2b628be8c3
Canna Wen <cannawen@gmail.com>
2023-04-11 20:45:11 -0400

    Make mock logs

---
+++ src/__mocks__/log.ts
@@ -0,0 +1,12 @@
+/* eslint-disable sort-keys */
+const log = {
+    error: jest.fn(),
+    warn: jest.fn(),
+    info: jest.fn(),
+    http: jest.fn(),
+    verbose: jest.fn(),
+    debug: jest.fn(),
+    silly: jest.fn(),
+};
+
+export default log;
