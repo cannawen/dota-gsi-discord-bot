b562cfa28dd6973557669212ab783b8a540d3b5b
Canna Wen <cannawen@gmail.com>
2023-04-12 08:03:13 -0400

    Add tests for stop audio

---
+++ src/effects/__tests__/stopAudio.test.ts
@@ -0,0 +1,46 @@
+import Fact from "../../engine/Fact";
+import { getResults } from "../../__tests__/helpers";
+import rule from "../stopAudio";
+
+const mockStop = jest.fn();
+
+describe("stopAudio", () => {
+    describe("true", () => {
+        let results: Fact<unknown>;
+        beforeEach(() => {
+            results = getResults(rule, {
+                stopAudio: true,
+                discordSubscriptionTopic: {
+                    player: {
+                        stop: mockStop,
+                    },
+                },
+            }) as Fact<unknown>;
+        });
+        test("reset state", () => {
+            expect(results).toContainFact("stopAudio", undefined);
+        });
+        test("play audio resource", () => {
+            expect(mockStop).toHaveBeenCalledTimes(1);
+        });
+    });
+    describe("false", () => {
+        let results: Fact<unknown>;
+        beforeEach(() => {
+            results = getResults(rule, {
+                stopAudio: false,
+                discordSubscriptionTopic: {
+                    player: {
+                        stop: mockStop,
+                    },
+                },
+            }) as Fact<unknown>;
+        });
+        test("reset state", () => {
+            expect(results).toContainFact("stopAudio", undefined);
+        });
+        test("play audio resource", () => {
+            expect(mockStop).toHaveBeenCalledTimes(0);
+        });
+    });
+});
