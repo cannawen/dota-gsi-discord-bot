d07435113497fd56417a5611b3ea0db39a572a4b
Canna Wen <cannawen@gmail.com>
2023-04-13 22:55:32 -0400

    Handle when to disable discord properly [#49]

--- src/discord/rules/__tests__/discordEnabled.test.ts
+++ src/discord/rules/__tests__/discordEnabled.test.ts
@@ -8,23 +8,39 @@ import rule from "../discordEnabled";
 import topics from "../../../topics";
 
 describe("discordEnabled", () => {
-    test("no one else already connected", () => {
-        (engine.getSessions as jest.Mock).mockReturnValue(new Map());
+    let map: Map<string, FactStore>;
+    beforeEach(() => {
+        map = new Map();
+        const factStore = new FactStore();
+        factStore.set(new Fact(topics.studentId, "studentId"));
+        factStore.set(new Fact(topics.discordGuildId, "guildId"));
+        factStore.set(new Fact(topics.discordGuildChannelId, "channelId"));
+        map.set("studentId", factStore);
+        (engine.getSessions as jest.Mock).mockReturnValue(map);
+    });
+    test("no one else connected to same guild+channel", () => {
+        const factStore = new FactStore();
+        factStore.set(new Fact(topics.studentId, "someone-else"));
+        factStore.set(new Fact(topics.discordGuildId, "guildId2"));
+        factStore.set(new Fact(topics.discordGuildChannelId, "channelId"));
+        map.set("someone-else", factStore);
+
         const result = getResults(rule, {
+            studentId: "studentId",
             discordGuildChannelId: "channelId",
             discordGuildId: "guildId",
         });
         expect(result).toContainFact("discordAudioEnabled", true);
     });
-    test("someone else already connected", () => {
-        const map = new Map();
+    test("someone else already connected to same guild+channel", () => {
         const factStore = new FactStore();
+        factStore.set(new Fact(topics.studentId, "someone-else"));
         factStore.set(new Fact(topics.discordGuildId, "guildId"));
         factStore.set(new Fact(topics.discordGuildChannelId, "channelId"));
         map.set("someone-else", factStore);
 
-        (engine.getSessions as jest.Mock).mockReturnValue(map);
         const result = getResults(rule, {
+            studentId: "studentId",
             discordGuildChannelId: "channelId",
             discordGuildId: "guildId",
         });
--- src/discord/rules/discordEnabled.ts
+++ src/discord/rules/discordEnabled.ts
@@ -5,19 +5,22 @@ import topics from "../../topics";
 
 export default new Rule(
     "discord/enabled",
-    [topics.discordGuildId, topics.discordGuildChannelId],
+    [topics.discordGuildId, topics.discordGuildChannelId, topics.studentId],
     (get) => {
+        const studentId = get(topics.studentId)!;
         const guildId = get(topics.discordGuildId)!;
         const channelId = get(topics.discordGuildChannelId)!;
 
         const alreadyConnected = Array.from(engine.getSessions()).reduce(
-            (memo, [studentId, db]) => {
+            (memo, [otherStudentId, db]) => {
+                if (otherStudentId === studentId) {
+                    return memo;
+                }
                 const existingGuildId = db.get(topics.discordGuildId);
                 const existingChannelId = db.get(topics.discordGuildChannelId);
                 return (
                     memo ||
-                    (studentId !== db.get(topics.studentId) &&
-                        existingGuildId === guildId &&
+                    (existingGuildId === guildId &&
                         existingChannelId === channelId)
                 );
             },
