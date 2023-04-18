f2a4697df8916bf29d260b6e4527ea59107c44ce
Canna Wen <cannawen@gmail.com>
2023-04-17 22:10:47 -0400

    Questions about how to reconcile async set facts vs. new facts triggered by outside factors

--- src/engine/Rule.ts
+++ src/engine/Rule.ts
@@ -1,6 +1,9 @@
 import Fact from "./Fact";
 import Topic from "./Topic";
 
+// TODO consider: now you can set fact on engine willy-nilly,
+// is it still worth to be able to return a promise of a fact from the rule engine?
+// How to reconcile there are two ways to set facts now?
 class Rule {
     // label is only used for logging purposes
     public readonly label: string;
