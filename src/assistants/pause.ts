0f27af1b600194721b56fb4d14dc85f9bf1c4f1e
Canna Wen <cannawen@gmail.com>
2023-04-18 13:10:25 -0400

    Only handle pauses when in-game
    (There were "Stop playing audio" logs unexpectedly when the game ended)

--- src/assistants/pause.ts
+++ src/assistants/pause.ts
@@ -13,13 +13,14 @@ export const defaultConfig = EffectConfig.PUBLIC_INTERRUPTING;
 export default new RuleConfigurable(
     rules.assistant.pause,
     configTopic,
-    [topics.paused],
+    [topics.paused, topics.inGame],
     (get, effect) => {
-        const paused = get(topics.paused)!;
-        if (paused) {
-            return new Fact(effect, "resources/audio/jeopardy.mp3");
-        } else {
-            return new Fact(topics.stopAudio, true);
+        if (get(topics.inGame)!) {
+            if (get(topics.paused)!) {
+                return new Fact(effect, "resources/audio/jeopardy.mp3");
+            } else {
+                return new Fact(topics.stopAudio, true);
+            }
         }
     }
 );
