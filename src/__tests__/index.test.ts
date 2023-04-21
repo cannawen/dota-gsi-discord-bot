0f7fcfe3fd400b74f745a56a8c9ecf64f9e7bca6
Canna Wen <cannawen@gmail.com>
2023-04-21 14:16:37 -0400

    Fix tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -12,7 +12,7 @@ describe("during app startup", () => {
     test("should register all rules with engine", () => {
         registerEverything();
 
-        const assistantCount = 17;
+        const assistantCount = 19;
         const assistantExcessRuleCount = 4;
         const discordRuleCount = 3;
         const effectRuleCount = 4;
