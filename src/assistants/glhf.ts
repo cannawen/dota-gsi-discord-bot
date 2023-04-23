fb6a7893733a7ebad5f1f4d3f69d0c0c0cfb1a74
Canna Wen <cannawen@gmail.com>
2023-04-22 20:09:37 -0400

    Fix regression from when we decided InGame rule decorator would not count time 0 as in game

--- src/assistants/glhf.ts
+++ src/assistants/glhf.ts
@@ -1,7 +1,6 @@
 import { EffectConfig } from "../effectConfigManager";
 import Fact from "../engine/Fact";
 import RuleConfigurable from "../engine/RuleConfigurable";
-import RuleDecoratorInGame from "../engine/RuleDecoratorInGame";
 import rules from "../rules";
 import topicManager from "../engine/topicManager";
 import topics from "../topics";
@@ -11,16 +10,13 @@ export const defaultConfig = EffectConfig.PRIVATE;
 export const assistantDescription =
     "Wishes you good fortune at the start of the game";
 
-export default new RuleDecoratorInGame(
-    new RuleConfigurable(
-        rules.assistant.glhf,
-        configTopic,
-        [topics.time],
-        (get, effect) => {
-            const time = get(topics.time)!;
-            if (time === 0) {
-                return new Fact(effect, "resources/audio/glhf.mp3");
-            }
+export default new RuleConfigurable(
+    rules.assistant.glhf,
+    configTopic,
+    [topics.time],
+    (get, effect) => {
+        if (get(topics.time)! === 0 && get(topics.inGame)! === true) {
+            return new Fact(effect, "resources/audio/glhf.mp3");
         }
-    )
+    }
 );
