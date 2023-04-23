1499229142be98dc885083d9b2f2db61159fda70
Canna Wen <cannawen@gmail.com>
2023-04-23 17:22:43 -0400

    Add test for in-game rule decorator

---
+++ src/engine/__tests__/RuleDecoratorInGame.test.ts
@@ -0,0 +1,39 @@
+import Fact from "../Fact";
+import { getResults } from "../../__tests__/helpers";
+import Rule from "../Rule";
+import RuleDecoratorInGame from "../RuleDecoratorInGame";
+import Topic from "../Topic";
+
+describe("RuleDecoratorInGame", () => {
+    let rule: RuleDecoratorInGame;
+    let topic: Topic<number>;
+
+    beforeEach(() => {
+        topic = new Topic<number>("topic");
+        rule = new RuleDecoratorInGame(
+            new Rule("test", [topic], (get) => new Fact(topic, 5))
+        );
+    });
+
+    describe("in game", () => {
+        test("time not 0", () => {
+            const results = getResults(rule, { inGame: true, time: 5 });
+            expect(results).toContainFact("topic", 5);
+        });
+        test("time 0", () => {
+            const results = getResults(rule, { inGame: true, time: 0 });
+            expect(results).toBeUndefined();
+        });
+    });
+
+    describe("not in game", () => {
+        test("time not 0", () => {
+            const results = getResults(rule, { inGame: false, time: 5 });
+            expect(results).toBeUndefined();
+        });
+        test("time 0", () => {
+            const results = getResults(rule, { inGame: false, time: 5 });
+            expect(results).toBeUndefined();
+        });
+    });
+});
