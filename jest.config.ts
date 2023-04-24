3a2e72495659c127195341e16ad8f6ee6a1280ea
Canna Wen <cannawen@gmail.com>
2023-04-23 23:19:51 -0400

    Use tsconfig.build.json so we do not transpile tests when building project

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     clearMocks: true,
-    // This is needed so we do not run *.test.js files
-    // Because the *.test.ts files are already being run
-    modulePathIgnorePatterns: ["<rootDir>/build/.+test.js$"],
     preset: "ts-jest",
     setupFiles: ["<rootDir>/.jest/setEnvVars.ts"],
     setupFilesAfterEnv: ["<rootDir>/.jest/jest.setup.ts"],
