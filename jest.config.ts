af6868a4e84bed8a5996c96d3b23774877df9354
Canna Wen <cannawen@gmail.com>
2023-04-23 22:39:12 -0400

    Have jest only test typescript files

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     clearMocks: true,
-    modulePathIgnorePatterns: ["helpers.ts"],
+    // This is needed so we do not test test.js files
+    // Because the test.ts files are already being run
+    modulePathIgnorePatterns: ["<rootDir>/build/.+test.js$"],
     preset: "ts-jest",
     setupFiles: ["<rootDir>/.jest/setEnvVars.js"],
     setupFilesAfterEnv: ["<rootDir>/.jest/jest.setup.ts"],
