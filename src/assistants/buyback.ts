04f5c7982b19eb6aa6ad7418ffb8841b6577047a
Canna Wen <cannawen@gmail.com>
2023-04-28 10:08:52 -0400

    Finish updating buyback rule

--- src/assistants/buyback.ts
+++ src/assistants/buyback.ts
@@ -19,14 +19,14 @@ const hasBuybackTopic = topicManager.createTopic<boolean>("hasBuybackTopic");
 
 export default [
     new Rule(
-        "when buyback is available after 30 minutes",
+        "when buyback is available",
         [topics.buybackCooldown, topics.gold, topics.buybackCost],
         () => {},
         ([cooldown]) => cooldown === 0,
         ([_, gold, cost]) => new Fact(hasBuybackTopic, gold >= cost)
     ),
     new Rule(
-        "when buyback is not available after 30 minutes",
+        "when buyback is not available",
         [topics.buybackCooldown, topics.gold, topics.buybackCost],
         () => {},
         ([cooldown]) => cooldown !== 0,
@@ -35,16 +35,10 @@ export default [
     new Rule(
         "warn about buyback",
         [hasBuybackTopic, topics.buybackCooldown],
-        (get) => {
-            const hasBuyback = get(hasBuybackTopic)!;
-            const buybackCooldown = get(topics.buybackCooldown)!;
-            if (!hasBuyback && buybackCooldown === 0) {
-                return new Fact(
-                    topics.configurableEffect,
-                    "you do not have buyback gold"
-                );
-            }
-        }
+        () => {},
+        ([hasBuyback, cooldown]) => !hasBuyback && cooldown === 0,
+        () =>
+            new Fact(topics.configurableEffect, "you do not have buyback gold")
     ),
 ]
     .map((rule) => new RuleDecoratorStartAndEndMinute(30, undefined, rule))
