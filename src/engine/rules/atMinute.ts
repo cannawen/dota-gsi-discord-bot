7822dda1aa6d88c01594e31408fcb12b192fb4cb
Canna Wen <cannawen@gmail.com>
2023-05-04 08:13:31 -0400

    Clean up at minute rule[#65]

--- src/engine/rules/atMinute.ts
+++ src/engine/rules/atMinute.ts
@@ -5,12 +5,5 @@ import Rule from "../Rule";
  * This rule will happen at `time` minutes when we are in a game.
  */
 export default function atMinute(time: number, rule: Rule) {
-    const startEndRule = betweenMinutes(time, time, rule);
-    return new Rule({
-        label: startEndRule.label,
-        trigger: startEndRule.trigger,
-        given: startEndRule.given,
-        when: startEndRule.when,
-        then: startEndRule.then,
-    });
+    return betweenMinutes(time, time, rule);
 }
