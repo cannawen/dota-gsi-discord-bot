83fec44656ef41a30fc3231faa90a598bd90c518
Canna Wen <cannawen@gmail.com>
2023-05-07 23:00:50 -0400

    Refactor healing lotus test to go through game states

--- src/assistants/__tests__/lotus.test.ts
+++ src/assistants/__tests__/lotus.test.ts
@@ -6,56 +6,53 @@ const params = {
     inGame: true,
     time: 3 * 60 - 15,
 };
-describe("healing lotus, in game", () => {
-    let initial: any;
-    beforeEach(() => {
-        initial = getResults(rule, params);
-    });
-    describe("3 minute", () => {
-        test("play sound", () => {
-            expect(initial).toContainAudioEffect(
-                "resources/audio/lotus-soon.mp3"
-            );
-        });
-    });
-    describe("11 minute", () => {
-        test("do not play sound", () => {
-            const results = getResults(
-                rule,
-                {
-                    ...params,
-                    time: 11 * 60 - 15,
-                },
-                initial
-            );
-            expect(results).not.toContainAudioEffect();
-        });
-    });
 
-    describe("6 minute", () => {
-        test("play sound", () => {
-            const results = getResults(
-                rule,
-                {
-                    ...params,
-                    time: 6 * 60 - 15,
-                },
-                initial
-            );
-            expect(results).toContainAudioEffect();
-        });
-    });
-    describe("15 minute", () => {
-        test("do not play sound", () => {
-            const results = getResults(
-                rule,
-                {
-                    ...params,
-                    time: 15 * 60 - 15,
-                },
-                initial
-            );
-            expect(results).not.toContainAudioEffect();
-        });
-    });
+test("healing lotus, in game", () => {
+    const threeMin = getResults(rule, params);
+    expect(threeMin).toContainAudioEffect("resources/audio/lotus-soon.mp3");
+    const sixMin = getResults(
+        rule,
+        {
+            ...params,
+            time: 6 * 60 - 15,
+        },
+        threeMin
+    );
+    expect(sixMin).toContainAudioEffect();
+    const nineMin = getResults(
+        rule,
+        {
+            ...params,
+            time: 9 * 60 - 15,
+        },
+        sixMin
+    );
+    expect(nineMin).toContainAudioEffect();
+    const elevenMin = getResults(
+        rule,
+        {
+            ...params,
+            time: 11 * 60 - 15,
+        },
+        nineMin
+    );
+    expect(elevenMin).not.toContainAudioEffect();
+    const twelveMin = getResults(
+        rule,
+        {
+            ...params,
+            time: 12 * 60 - 15,
+        },
+        elevenMin
+    );
+    expect(twelveMin).toContainAudioEffect();
+    const fifteenMin = getResults(
+        rule,
+        {
+            ...params,
+            time: 15 * 60 - 15,
+        },
+        twelveMin
+    );
+    expect(fifteenMin).not.toContainAudioEffect();
 });
