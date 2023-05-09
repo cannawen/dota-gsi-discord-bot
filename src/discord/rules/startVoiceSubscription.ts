00939ab6d2bcef0471668dab5314d0e9f4cb7c38
Canna Wen <cannawen@gmail.com>
2023-05-09 14:58:12 -0400

    Re-enable STT, I'm sure it will be fine

--- src/discord/rules/startVoiceSubscription.ts
+++ src/discord/rules/startVoiceSubscription.ts
@@ -134,27 +134,27 @@ export default new Rule({
             );
         });
 
-        // connection.receiver.speaking.on("start", (userId) => {
-        //     stt.transcribe(connection.receiver, userId)
-        //         .then((utterance) => {
-        //             if (!utterance) return;
-        //             // If I am speaking, log content. If anyone else is, do not log. Because this is creepy.
-        //             if (userId === "169619011238232073") {
-        //                 log.info("tts", utterance);
-        //             }
-        //             engine.setFact(
-        //                 studentId,
-        //                 new Fact(topics.lastDiscordUtterance, utterance)
-        //             );
-        //         })
-        //         .catch((error) => {
-        //             log.error(
-        //                 "tts",
-        //                 "Problem with transcription, %s",
-        //                 error.message
-        //             );
-        //         });
-        // });
+        connection.receiver.speaking.on("start", (userId) => {
+            stt.transcribe(connection.receiver, userId)
+                .then((utterance) => {
+                    if (!utterance) return;
+                    // If I am speaking, log content. If anyone else is, do not log. Because this is creepy.
+                    if (userId === "169619011238232073") {
+                        log.info("tts", utterance);
+                    }
+                    engine.setFact(
+                        studentId,
+                        new Fact(topics.lastDiscordUtterance, utterance)
+                    );
+                })
+                .catch((error) => {
+                    log.error(
+                        "tts",
+                        "Problem with transcription, %s",
+                        error.message
+                    );
+                });
+        });
 
         const player = Voice.createAudioPlayer();
         player.on("stateChange", (oldState, newState) => {
