0893036dc5ac31ba12ba808dd1fc5615d00a5bed
Canna Wen <cannawen@gmail.com>
2023-05-16 08:59:04 -0400

    Use conditional rule for neutral item reminder [#74]

--- src/assistants/__tests__/neutralItemReminder.test.ts
+++ src/assistants/__tests__/neutralItemReminder.test.ts
@@ -22,32 +22,30 @@ const HAS_TIER_2_NEUTRAL_ITEM = new PlayerItems(
     new Item("item_philosophers_stone", "Philosopher's Stone")
 ) as DeepReadonly<PlayerItems>;
 
+const params = {
+    inGame: true,
+    [rules.assistant.neutralItemReminder]: "PRIVATE",
+    items: NO_ITEMS,
+    time: NEUTRAL_ITEM_REMINDER_START_MINUTE * 60,
+};
+
 describe("neutralItemReminder", () => {
     describe("does not have neutral item", () => {
         test("do not warn before 10 minutes", () => {
             const result = getResults(rule, {
-                [rules.assistant.neutralItemReminder]: "PRIVATE",
+                ...params,
                 time: (NEUTRAL_ITEM_REMINDER_START_MINUTE - 1) * 60,
-                inGame: true,
-                items: NO_ITEMS,
             });
             expect(result).not.toContainAudioEffect();
         });
 
         test("warn after 2 minutes grace", () => {
-            const state = getResults(rule, {
-                [rules.assistant.neutralItemReminder]: "PRIVATE",
-                time: NEUTRAL_ITEM_REMINDER_START_MINUTE * 60,
-                inGame: true,
-                items: NO_ITEMS,
-            });
+            const state = getResults(rule, params, undefined, true);
             const result = getResults(
                 rule,
                 {
-                    [rules.assistant.neutralItemReminder]: "PRIVATE",
+                    ...params,
                     time: (NEUTRAL_ITEM_REMINDER_START_MINUTE + 2) * 60,
-                    inGame: true,
-                    items: NO_ITEMS,
                 },
                 state
             );
@@ -57,10 +55,8 @@ describe("neutralItemReminder", () => {
             const result2 = getResults(
                 rule,
                 {
-                    [rules.assistant.neutralItemReminder]: "PRIVATE",
+                    ...params,
                     time: (NEUTRAL_ITEM_REMINDER_START_MINUTE + 4) * 60,
-                    inGame: true,
-                    items: NO_ITEMS,
                 },
                 result
             );
@@ -73,17 +69,14 @@ describe("neutralItemReminder", () => {
     describe("gets neutral item within 1 minute", () => {
         test("should not warn", () => {
             const state1 = getResults(rule, {
-                [rules.assistant.neutralItemReminder]: "PRIVATE",
+                ...params,
                 time: NEUTRAL_ITEM_REMINDER_START_MINUTE * 60,
-                inGame: true,
-                items: NO_ITEMS,
             });
             const state2 = getResults(
                 rule,
                 {
-                    [rules.assistant.neutralItemReminder]: "PRIVATE",
+                    ...params,
                     time: (NEUTRAL_ITEM_REMINDER_START_MINUTE + 1) * 60,
-                    inGame: true,
                     items: HAS_TIER_2_NEUTRAL_ITEM,
                 },
                 state1
@@ -91,9 +84,8 @@ describe("neutralItemReminder", () => {
             const result = getResults(
                 rule,
                 {
-                    [rules.assistant.neutralItemReminder]: "PRIVATE",
+                    ...params,
                     time: 9 * 60,
-                    inGame: true,
                     items: HAS_TIER_2_NEUTRAL_ITEM,
                 },
                 state2
@@ -106,17 +98,15 @@ describe("neutralItemReminder", () => {
         describe("is the same tier", () => {
             test("do not remind", () => {
                 const state1 = getResults(rule, {
-                    [rules.assistant.neutralItemReminder]: "PRIVATE",
+                    ...params,
                     time: 18 * 60, // Tier 2 zone
-                    inGame: true,
                     items: HAS_TIER_2_NEUTRAL_ITEM,
                 });
                 const state2 = getResults(
                     rule,
                     {
-                        [rules.assistant.neutralItemReminder]: "PRIVATE",
+                        ...params,
                         time: 20 * 60, // Tier 2 zone
-                        inGame: true,
                         items: HAS_TIER_2_NEUTRAL_ITEM,
                     },
                     state1
@@ -127,17 +117,15 @@ describe("neutralItemReminder", () => {
         describe("is one tier below", () => {
             test("do not remind", () => {
                 const state1 = getResults(rule, {
-                    [rules.assistant.neutralItemReminder]: "PRIVATE",
+                    ...params,
                     time: 28 * 60, // Tier 3 zone
-                    inGame: true,
                     items: HAS_TIER_2_NEUTRAL_ITEM,
                 });
                 const state2 = getResults(
                     rule,
                     {
-                        [rules.assistant.neutralItemReminder]: "PRIVATE",
+                        ...params,
                         time: 30 * 60, // Tier 3 zone
-                        inGame: true,
                         items: HAS_TIER_2_NEUTRAL_ITEM,
                     },
                     state1
@@ -148,17 +136,15 @@ describe("neutralItemReminder", () => {
         describe("is 2 tiers below", () => {
             test("remind to get a better neutral", () => {
                 const state1 = getResults(rule, {
-                    [rules.assistant.neutralItemReminder]: "PRIVATE",
+                    ...params,
                     time: 38 * 60, // Tier 4 zone
-                    inGame: true,
                     items: HAS_TIER_2_NEUTRAL_ITEM,
                 });
                 const state2 = getResults(
                     rule,
                     {
-                        [rules.assistant.neutralItemReminder]: "PRIVATE",
+                        ...params,
                         time: 40 * 60, // Tier 4 zone
-                        inGame: true,
                         items: HAS_TIER_2_NEUTRAL_ITEM,
                     },
                     state1
@@ -171,17 +157,15 @@ describe("neutralItemReminder", () => {
         describe("is 3 tiers below", () => {
             test("remind to get a better neutral", () => {
                 const state1 = getResults(rule, {
-                    [rules.assistant.neutralItemReminder]: "PRIVATE",
+                    ...params,
                     time: 60 * 60, // Tier 5 zone
-                    inGame: true,
                     items: HAS_TIER_2_NEUTRAL_ITEM,
                 });
                 const state2 = getResults(
                     rule,
                     {
-                        [rules.assistant.neutralItemReminder]: "PRIVATE",
+                        ...params,
                         time: 62 * 60, // Tier 5 zone
-                        inGame: true,
                         items: HAS_TIER_2_NEUTRAL_ITEM,
                     },
                     state1
--- src/assistants/neutralItemReminder.ts
+++ src/assistants/neutralItemReminder.ts
@@ -1,4 +1,4 @@
-import betweenSeconds from "../engine/rules/betweenSeconds";
+import conditionalEveryIntervalSeconds from "../engine/rules/conditionalEveryIntervalSeconds";
 import ConfigInfo from "../ConfigInfo";
 import configurable from "../engine/rules/configurable";
 import EffectConfig from "../effects/EffectConfig";
@@ -6,7 +6,6 @@ import Fact from "../engine/Fact";
 import helper from "./helpers/neutralItems";
 import Rule from "../engine/Rule";
 import rules from "../rules";
-import topicManager from "../engine/topicManager";
 import topics from "../topics";
 
 export const configInfo = new ConfigInfo(
@@ -19,50 +18,27 @@ export const configInfo = new ConfigInfo(
 const TIME_BETWEEN_REMINDERS = 120;
 const NEUTRAL_ITEM_REMINDER_START_TIME = 10 * 60;
 
-const lastReminderTimeTopic = topicManager.createTopic<number>(
-    "lastNeutralItemReminderTimeTopic"
-);
-
-export default betweenSeconds(
-    NEUTRAL_ITEM_REMINDER_START_TIME,
-    undefined,
-    configurable(
-        configInfo.ruleIndentifier,
-        new Rule({
-            label: rules.assistant.neutralItemReminder,
-            trigger: [topics.items, topics.time],
-            given: [lastReminderTimeTopic],
-            then: ([items, time], [lastReminderTime]) => {
-                const appropriateItem = helper.isItemAppropriateForTime(
-                    items.neutral?.id,
-                    time
-                );
-
-                // If we have an appropriate neutral item
-                if (appropriateItem) {
-                    // Clear reminder time
-                    return new Fact(lastReminderTimeTopic, undefined);
-                } else {
-                    // If we do not have an appropriate nuetral item
-                    // If we have never reminded them before
-                    if (!lastReminderTime) {
-                        // Set reminder time without effect to give them some grace
-                        return new Fact(lastReminderTimeTopic, time);
-                    }
-                    // If we have been silent for the grace time
-                    // But they still do not have an appropriate neutral item
-                    if (time - lastReminderTime >= TIME_BETWEEN_REMINDERS) {
-                        const audio = items.neutral
-                            ? "you should upgrade your neutral item"
-                            : "you do not have a neutral item";
-                        // Remind them and update reminder time
-                        return [
-                            new Fact(topics.configurableEffect, audio),
-                            new Fact(lastReminderTimeTopic, time),
-                        ];
-                    }
-                }
-            },
-        })
+export default [
+    new Rule({
+        label: rules.assistant.neutralItemReminder,
+        trigger: [topics.items, topics.time],
+        then: ([items]) => {
+            const audio = items.neutral
+                ? "you should upgrade your neutral item"
+                : "you do not have a neutral item";
+            // Remind them and update reminder time
+            return [new Fact(topics.configurableEffect, audio)];
+        },
+    }),
+]
+    .map((rule) =>
+        conditionalEveryIntervalSeconds(
+            NEUTRAL_ITEM_REMINDER_START_TIME,
+            undefined,
+            ([items, time]) =>
+                !helper.isItemAppropriateForTime(items.neutral?.id, time),
+            TIME_BETWEEN_REMINDERS,
+            rule
+        )
     )
-);
+    .map((rule) => configurable(configInfo.ruleIndentifier, rule));
