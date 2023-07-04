1d8de2097291c6d2055093bd915cbbdbbae3d8f3
Canna Wen <cannawen@gmail.com>
2023-07-04 16:38:50 -0400

    Warn once when a tower falls below 500 health [#114] Apparently we no longer get the data for destroyed buildings

--- src/assistants/freeFortification.ts
+++ src/assistants/freeFortification.ts
@@ -15,11 +15,7 @@ export const configInfo = new ConfigInfo(
     EffectConfig.PUBLIC_INTERRUPTING
 );
 
-const TOWER_HEALTH_WARN_THRESHOLD = 150;
-
-const t1sAllUpTopic = new Topic<boolean>("t1sAllUpTopic");
-const t2sAllUpTopic = new Topic<boolean>("t2sAllUpTopic");
-const t3sAllUpTopic = new Topic<boolean>("t3sAllUpTopic");
+const TOWER_HEALTH_WARN_THRESHOLD = 500;
 
 const t1FreeFortWarnedAlready = new Topic<boolean>("t1FreeFortWarnedAlready");
 const t2FreeFortWarnedAlready = new Topic<boolean>("t2FreeFortWarnedAlready");
@@ -27,35 +23,11 @@ const t3FreeFortWarnedAlready = new Topic<boolean>("t3FreeFortWarnedAlready");
 
 export default [
     new Rule({
-        label: "t1s all up",
-        trigger: [topics.t1Top, topics.t1Mid, topics.t1Bot],
-        then: ([top, mid, bot]) =>
-            new Fact(t1sAllUpTopic, top > 0 && mid > 0 && bot > 0),
-    }),
-    new Rule({
-        label: "t2s all up",
-        trigger: [topics.t2Top, topics.t2Mid, topics.t2Bot],
-        then: ([top, mid, bot]) =>
-            new Fact(t2sAllUpTopic, top > 0 && mid > 0 && bot > 0),
-    }),
-    new Rule({
-        label: "t3s all up",
-        trigger: [topics.t3Top, topics.t3Mid, topics.t3Bot],
-        then: ([top, mid, bot]) =>
-            new Fact(t3sAllUpTopic, top > 0 && mid > 0 && bot > 0),
-    }),
-    new Rule({
         label: "free fort for t1",
-        trigger: [
-            topics.t1Top,
-            topics.t1Mid,
-            topics.t1Bot,
-            t1sAllUpTopic,
-            t1FreeFortWarnedAlready,
-        ],
-        when: ([top, mid, bot, free, warnedAlready]) =>
+        trigger: [topics.t1Top, topics.t1Mid, topics.t1Bot],
+        given: [t1FreeFortWarnedAlready],
+        when: ([top, mid, bot], [warnedAlready]) =>
             !warnedAlready &&
-            free &&
             (top < TOWER_HEALTH_WARN_THRESHOLD ||
                 mid < TOWER_HEALTH_WARN_THRESHOLD ||
                 bot < TOWER_HEALTH_WARN_THRESHOLD),
@@ -66,16 +38,10 @@ export default [
     }),
     new Rule({
         label: "free fort for t2",
-        trigger: [
-            topics.t2Top,
-            topics.t2Mid,
-            topics.t2Bot,
-            t2sAllUpTopic,
-            t2FreeFortWarnedAlready,
-        ],
-        when: ([top, mid, bot, free, warnedAlready]) =>
+        trigger: [topics.t2Top, topics.t2Mid, topics.t2Bot],
+        given: [t2FreeFortWarnedAlready],
+        when: ([top, mid, bot], [warnedAlready]) =>
             !warnedAlready &&
-            free &&
             (top < TOWER_HEALTH_WARN_THRESHOLD ||
                 mid < TOWER_HEALTH_WARN_THRESHOLD ||
                 bot < TOWER_HEALTH_WARN_THRESHOLD),
@@ -86,16 +52,10 @@ export default [
     }),
     new Rule({
         label: "free fort for t3",
-        trigger: [
-            topics.t3Top,
-            topics.t3Mid,
-            topics.t3Bot,
-            t3sAllUpTopic,
-            t3FreeFortWarnedAlready,
-        ],
-        when: ([top, mid, bot, free, warnedAlready]) =>
+        trigger: [topics.t3Top, topics.t3Mid, topics.t3Bot],
+        given: [t3FreeFortWarnedAlready],
+        when: ([top, mid, bot], [warnedAlready]) =>
             !warnedAlready &&
-            free &&
             (top < TOWER_HEALTH_WARN_THRESHOLD ||
                 mid < TOWER_HEALTH_WARN_THRESHOLD ||
                 bot < TOWER_HEALTH_WARN_THRESHOLD),
