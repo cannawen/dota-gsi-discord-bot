98093b9ec8211b307946e8644c934dd8b0d1cc1a
Canna Wen <cannawen@gmail.com>
2023-07-04 09:58:07 -0400

    Warn once when a tower falls below 150 health [#114] Refactor tower state to not include max health

--- src/gsi/gsiBuildings.ts
+++ src/gsi/gsiBuildings.ts
@@ -12,105 +12,105 @@ export default new Rule({
         const buildings = (data as GsiData).buildings;
         if (buildings?.radiant) {
             return [
-                new Fact(topics.t1Top, [
-                    buildings?.radiant.dota_goodguys_tower1_top.health,
-                    buildings?.radiant.dota_goodguys_tower1_top.maxHealth,
-                ]),
-                new Fact(topics.t2Top, [
-                    buildings?.radiant.dota_goodguys_tower2_top.health,
-                    buildings?.radiant.dota_goodguys_tower2_top.maxHealth,
-                ]),
-                new Fact(topics.t3Top, [
-                    buildings?.radiant.dota_goodguys_tower3_top.health,
-                    buildings?.radiant.dota_goodguys_tower3_top.maxHealth,
-                ]),
-                new Fact(topics.t4Top, [
-                    buildings?.radiant.dota_goodguys_tower4_top.health,
-                    buildings?.radiant.dota_goodguys_tower4_top.maxHealth,
-                ]),
-                new Fact(topics.t1Mid, [
-                    buildings?.radiant.dota_goodguys_tower1_mid.health,
-                    buildings?.radiant.dota_goodguys_tower1_mid.maxHealth,
-                ]),
-                new Fact(topics.t2Mid, [
-                    buildings?.radiant.dota_goodguys_tower2_mid.health,
-                    buildings?.radiant.dota_goodguys_tower2_mid.maxHealth,
-                ]),
-                new Fact(topics.t3Mid, [
-                    buildings?.radiant.dota_goodguys_tower3_mid.health,
-                    buildings?.radiant.dota_goodguys_tower3_mid.maxHealth,
-                ]),
-                new Fact(topics.t1Bot, [
-                    buildings?.radiant.dota_goodguys_tower1_bot.health,
-                    buildings?.radiant.dota_goodguys_tower1_bot.maxHealth,
-                ]),
-                new Fact(topics.t2Bot, [
-                    buildings?.radiant.dota_goodguys_tower2_bot.health,
-                    buildings?.radiant.dota_goodguys_tower2_bot.maxHealth,
-                ]),
-                new Fact(topics.t3Bot, [
-                    buildings?.radiant.dota_goodguys_tower3_bot.health,
-                    buildings?.radiant.dota_goodguys_tower3_bot.maxHealth,
-                ]),
-                new Fact(topics.t4Bot, [
-                    buildings?.radiant.dota_goodguys_tower4_bot.health,
-                    buildings?.radiant.dota_goodguys_tower4_bot.maxHealth,
-                ]),
-                new Fact(topics.ancient, [
-                    buildings?.radiant.dota_goodguys_fort.health,
-                    buildings?.radiant.dota_goodguys_fort.maxHealth,
-                ]),
+                new Fact(
+                    topics.t1Top,
+                    buildings?.radiant.dota_goodguys_tower1_top.health
+                ),
+                new Fact(
+                    topics.t2Top,
+                    buildings?.radiant.dota_goodguys_tower2_top.health
+                ),
+                new Fact(
+                    topics.t3Top,
+                    buildings?.radiant.dota_goodguys_tower3_top.health
+                ),
+                new Fact(
+                    topics.t4Top,
+                    buildings?.radiant.dota_goodguys_tower4_top.health
+                ),
+                new Fact(
+                    topics.t1Mid,
+                    buildings?.radiant.dota_goodguys_tower1_mid.health
+                ),
+                new Fact(
+                    topics.t2Mid,
+                    buildings?.radiant.dota_goodguys_tower2_mid.health
+                ),
+                new Fact(
+                    topics.t3Mid,
+                    buildings?.radiant.dota_goodguys_tower3_mid.health
+                ),
+                new Fact(
+                    topics.t1Bot,
+                    buildings?.radiant.dota_goodguys_tower1_bot.health
+                ),
+                new Fact(
+                    topics.t2Bot,
+                    buildings?.radiant.dota_goodguys_tower2_bot.health
+                ),
+                new Fact(
+                    topics.t3Bot,
+                    buildings?.radiant.dota_goodguys_tower3_bot.health
+                ),
+                new Fact(
+                    topics.t4Bot,
+                    buildings?.radiant.dota_goodguys_tower4_bot.health
+                ),
+                new Fact(
+                    topics.ancient,
+                    buildings?.radiant.dota_goodguys_fort.health
+                ),
             ];
         } else if (buildings?.dire) {
             return [
-                new Fact(topics.t1Top, [
-                    buildings?.dire.dota_badguys_tower1_top.health,
-                    buildings?.dire.dota_badguys_tower1_top.maxHealth,
-                ]),
-                new Fact(topics.t2Top, [
-                    buildings?.dire.dota_badguys_tower2_top.health,
-                    buildings?.dire.dota_badguys_tower2_top.maxHealth,
-                ]),
-                new Fact(topics.t3Top, [
-                    buildings?.dire.dota_badguys_tower3_top.health,
-                    buildings?.dire.dota_badguys_tower3_top.maxHealth,
-                ]),
-                new Fact(topics.t4Top, [
-                    buildings?.dire.dota_badguys_tower4_top.health,
-                    buildings?.dire.dota_badguys_tower4_top.maxHealth,
-                ]),
-                new Fact(topics.t1Mid, [
-                    buildings?.dire.dota_badguys_tower1_mid.health,
-                    buildings?.dire.dota_badguys_tower1_mid.maxHealth,
-                ]),
-                new Fact(topics.t2Mid, [
-                    buildings?.dire.dota_badguys_tower2_mid.health,
-                    buildings?.dire.dota_badguys_tower2_mid.maxHealth,
-                ]),
-                new Fact(topics.t3Mid, [
-                    buildings?.dire.dota_badguys_tower3_mid.health,
-                    buildings?.dire.dota_badguys_tower3_mid.maxHealth,
-                ]),
-                new Fact(topics.t1Bot, [
-                    buildings?.dire.dota_badguys_tower1_bot.health,
-                    buildings?.dire.dota_badguys_tower1_bot.maxHealth,
-                ]),
-                new Fact(topics.t2Bot, [
-                    buildings?.dire.dota_badguys_tower2_bot.health,
-                    buildings?.dire.dota_badguys_tower2_bot.maxHealth,
-                ]),
-                new Fact(topics.t3Bot, [
-                    buildings?.dire.dota_badguys_tower3_bot.health,
-                    buildings?.dire.dota_badguys_tower3_bot.maxHealth,
-                ]),
-                new Fact(topics.t4Bot, [
-                    buildings?.dire.dota_badguys_tower4_bot.health,
-                    buildings?.dire.dota_badguys_tower4_bot.maxHealth,
-                ]),
-                new Fact(topics.ancient, [
-                    buildings?.dire.dota_badguys_fort.health,
-                    buildings?.dire.dota_badguys_fort.maxHealth,
-                ]),
+                new Fact(
+                    topics.t1Top,
+                    buildings?.dire.dota_badguys_tower1_top.health
+                ),
+                new Fact(
+                    topics.t2Top,
+                    buildings?.dire.dota_badguys_tower2_top.health
+                ),
+                new Fact(
+                    topics.t3Top,
+                    buildings?.dire.dota_badguys_tower3_top.health
+                ),
+                new Fact(
+                    topics.t4Top,
+                    buildings?.dire.dota_badguys_tower4_top.health
+                ),
+                new Fact(
+                    topics.t1Mid,
+                    buildings?.dire.dota_badguys_tower1_mid.health
+                ),
+                new Fact(
+                    topics.t2Mid,
+                    buildings?.dire.dota_badguys_tower2_mid.health
+                ),
+                new Fact(
+                    topics.t3Mid,
+                    buildings?.dire.dota_badguys_tower3_mid.health
+                ),
+                new Fact(
+                    topics.t1Bot,
+                    buildings?.dire.dota_badguys_tower1_bot.health
+                ),
+                new Fact(
+                    topics.t2Bot,
+                    buildings?.dire.dota_badguys_tower2_bot.health
+                ),
+                new Fact(
+                    topics.t3Bot,
+                    buildings?.dire.dota_badguys_tower3_bot.health
+                ),
+                new Fact(
+                    topics.t4Bot,
+                    buildings?.dire.dota_badguys_tower4_bot.health
+                ),
+                new Fact(
+                    topics.ancient,
+                    buildings?.dire.dota_badguys_fort.health
+                ),
             ];
         }
     },
