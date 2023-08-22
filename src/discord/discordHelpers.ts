e95a8ffafbcf32ece9d6c631603e7867b9c7bd6a
Canna Wen <cannawen@gmail.com>
2023-08-22 00:49:57 -0400

    Add number of people connected to guild function back to discord helpers

--- src/discord/discordHelpers.ts
+++ src/discord/discordHelpers.ts
@@ -1,6 +1,9 @@
 import { CacheType, ChatInputCommandInteraction } from "discord.js";
 import CryptoJS from "crypto-js";
+import Engine from "../engine/Engine";
+import engine from "../customEngine";
 import log from "../log";
+import topics from "../topics";
 
 function hashUserId(userId: string): string {
     const key = process.env.STUDENT_ID_HASH_PRIVATE_KEY;
@@ -25,6 +28,13 @@ function studentId(input: ChatInputCommandInteraction<CacheType> | string) {
     }
 }
 
+function numberOfPeopleConnected(guildId: string): number {
+    return Array.from(engine.getSessions().values()).filter(
+        (db) => Engine.get(db, topics.discordGuildId) === guildId
+    ).length;
+}
+
 export default {
+    numberOfPeopleConnected,
     studentId,
 };
