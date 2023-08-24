cannawen/dota-gsi-discord-bot

commit 299837a12012948ea0adffec83ee6b14122fb24c

Refactor to register rules only once

src/effectConfigManager.ts
@@ -24,6 +24,18 @@ function getDefaultConfigInfo(): ConfigInfo[] {
 }
 
 const defaultConfigInfo = getDefaultConfigInfo();
+defaultConfigInfo.map((configInfo) => {
+    const topic = topicManager.findTopic<EffectConfig>(
+        configInfo.ruleIndentifier
+    );
+    engine.register(
+        new Rule({
+            label: `update frontend when ${configInfo.ruleIndentifier} config is changed`,
+            trigger: [topic],
+            then: () => new Fact(topics.updateFrontend, true),
+        })
+    );
+});
 
 function effectFromString(effect: string) {
     switch (effect) {
@@ -49,13 +61,6 @@ function defaultConfigFacts(): Fact<EffectConfig | undefined>[] {
     return defaultConfigInfo.map((configInfo) => {
         const topic = topicManager.findTopic<EffectConfig>(
             configInfo.ruleIndentifier
-        );
-        engine.register(
-            new Rule({
-                label: `update frontend when ${configInfo.ruleIndentifier} config is changed`,
-                trigger: [topic],
-                then: () => new Fact(topics.updateFrontend, true),
-            })
         );
         topic.defaultValue = configInfo.defaultConfig;
         return new Fact(topic, undefined);

commit 1e9565e06072d868c7f0d29be26052e9a982ab0a

Fix oncetoken to register all frontend rules instead of just first one Regression introduced by commit 299837a12012948ea0adffec83ee6b14122fb24c that caused the frontend not to be updated when preferences are changed

src/effectConfigManager.ts
@@ -48,24 +48,27 @@ function effectFromString(effect: string) {
 let onceToken = true;
 
 function defaultConfigFacts(): Fact<EffectConfig | undefined>[] {
-    return defaultConfigInfo.map((configInfo) => {
+    const configFacts = defaultConfigInfo.map((configInfo) => {
         const topic = topicManager.findTopic<EffectConfig>(
             configInfo.ruleIndentifier
         );
-
-        if (onceToken) {
-            onceToken = false;
+        topic.defaultValue = configInfo.defaultConfig;
+        return new Fact(topic, undefined);
+    });
+    if (onceToken) {
+        onceToken = false;
+        configFacts.forEach((fact) => {
+            const topic = fact.topic;
             engine.register(
                 new Rule({
-                    label: `update frontend when ${configInfo.ruleIndentifier} config is changed`,
+                    label: `update frontend when ${topic.label} config is changed`,
                     trigger: [topic],
                     then: () => new Fact(topics.updateFrontend, true),
                 })
             );
-        }
-        topic.defaultValue = configInfo.defaultConfig;
-        return new Fact(topic, undefined);
-    });
+        });
+    }
+    return configFacts;
 }
 
 export default {