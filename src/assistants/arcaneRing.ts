8a89b455606f16f1ee3c7bb46eed2fe71f3cc5a8
Canna Wen <cannawen@gmail.com>
2023-08-27 23:28:27 -0400

    Add reminder to use arcane ring

---
+++ src/assistants/arcaneRing.ts
@@ -0,0 +1,35 @@
+import conditionalEveryIntervalSeconds from "../engine/rules/conditionalEveryIntervalSeconds";
+import ConfigInfo from "../ConfigInfo";
+import configurable from "../engine/rules/configurable";
+import EffectConfig from "../effects/EffectConfig";
+import Fact from "../engine/Fact";
+import Rule from "../engine/Rule";
+import rules from "../rules";
+import topics from "../topics";
+
+export const configInfo = new ConfigInfo(
+    rules.assistant.arcaneBoots,
+    "Arcane ring",
+    "Reminds you when your arcane ring is off cooldown and you are missing mana",
+    EffectConfig.PRIVATE
+);
+
+const REMINDER_INTERVAL_SECONDS = 30;
+const MANA_RESTORED_ON_CAST = 75;
+
+export default [
+    new Rule({
+        label: "arcane ring",
+        trigger: [topics.items],
+        given: [topics.alive, topics.mana, topics.maxMana],
+        when: ([items], [alive, mana, maxMana]) =>
+            alive &&
+            items.findItem("item_arcane_ring")?.canCast &&
+            maxMana - mana >= MANA_RESTORED_ON_CAST,
+        then: () => new Fact(topics.configurableEffect, "arcane ring"),
+    }),
+]
+    .map((rule) =>
+        conditionalEveryIntervalSeconds(REMINDER_INTERVAL_SECONDS, rule)
+    )
+    .map((rule) => configurable(configInfo.ruleIndentifier, rule));
