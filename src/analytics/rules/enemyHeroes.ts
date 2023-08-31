cd1dc051e78806bee768f73856894a07550b360c
Canna Wen <cannawen@gmail.com>
2023-08-31 08:41:25 -0400

    Only send analytics for enemy heroes before 10 minutes [#135]

--- src/analytics/rules/enemyHeroes.ts
+++ src/analytics/rules/enemyHeroes.ts
@@ -8,11 +8,14 @@ export default [
         label: "analytics/enemy_heroes_time",
         trigger: [topics.allEnemyHeroes],
         given: [topics.time, topics.studentId],
+        // Analytics being called on end game as well for unknown reasons
+        // Only trigger this analytics event if we parse heroes before 10 minutes
+        when: (_, [time]) => time < 10 * 60,
         then: ([heroes], [time, studentId]) => {
             analytics.mixpanel?.track("enemy heroes", {
                 distinct_id: studentId,
                 heroes: heroes,
-                gameTime: time || "no time available",
+                gameTime: time,
             });
         },
     }),
