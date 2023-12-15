d393b833fcee6233c3ae5bbbdeb2bd41fd502f78
Canna Wen <cannawen@gmail.com>
2023-12-14 19:47:41 -0500

    Have friendly and enemy heroes persist across restarts

--- src/topics.ts
+++ src/topics.ts
@@ -48,11 +48,17 @@ const gsi = {
     time: new Topic<number>("time"),
     paused: new Topic<boolean>("paused"),
     // map + minimap
-    allFriendlyHeroes: new Topic<DeepReadonly<string[]>>(
+    allFriendlyHeroes: new PersistentTopic<DeepReadonly<string[]>>(
         "allFriendlyHeroes",
-        []
+        { defaultValue: [], persistAcrossRestarts: true }
     ),
-    allEnemyHeroes: new Topic<DeepReadonly<string[]>>("allEnemyHeroes", []),
+    allEnemyHeroes: new PersistentTopic<DeepReadonly<string[]>>(
+        "allEnemyHeroes",
+        {
+            defaultValue: [],
+            persistAcrossRestarts: true,
+        }
+    ),
     // player
     gold: new Topic<number>("gold"),
     team: new Topic<string>("team", "radiant"), // if we are an observer, default to pretending to be on radiant side
