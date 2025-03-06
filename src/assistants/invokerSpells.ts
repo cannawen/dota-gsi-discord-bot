e5b671d5a9d392f18d5c7468643b612a3315e0f3
Canna Wen <cannawen@gmail.com>
2025-03-06 12:21:39 -0500

    Refactor invoker assistant for reusability. The TTS is not very good at reading letters

--- src/assistants/invokerSpells.ts
+++ src/assistants/invokerSpells.ts
@@ -8,35 +8,42 @@ import topics from "../topics";
 
 export const configInfo = new ConfigInfo(
     rules.assistant.invoker,
-    "Invoker voice commands",
+    "Invoker voice commands (BETA)",
     `Say "invoke 'spell name'" (i.e. tornado), and the bot will respond with the required orbs to invoke it (q w w)`,
     EffectConfig.NONE
 );
 
 const keywords = ["invoke", "invoker", "info"];
 
-export default [
-    ["W. W. W.", ["emp", "bmp"]],
-    ["Q. W. W.", "tornado"],
-    ["W. W. E.", "alacrity"],
-    ["Q. Q. W.", "ghost walk"],
-    ["Q. W. E.", "deafening blast"],
-    ["W. E. E.", ["chaos meteor", "meteor"]],
-    ["Q. Q. Q.", ["cold snap", "code snap"]],
-    ["Q. Q. E.", "ice wall"],
+const W = "double you,";
+const Q = "cue,";
+const E = "E,";
+
+const invokerSpells: Array<[string[], string|string[]]> = [
+    [[W, W, W], ["emp", "bmp"]],
+    [[Q, W, W], "tornado"],
+    [[W, W, E], "alacrity"],
+    [[Q, Q, W], "ghost walk"],
+    [[Q, W, E], "deafening (past|blast)"],
+    [[W, E, E], ["chaos meteor", "meteor", "meatball", "chaos media"]],
+    [[Q, Q, Q], ["cold snap", "code snap"]],
+    [[Q, Q, E], "ice wall"],
+    [[E, E, E], ["sun ?strike", "cataclysm", "send straight", "sunstroke", "some strike"]],
     [
-        "Q. E. E.",
+        [Q, E, E],
         [
             "forge spirits?",
             "forest spirits?",
             "george spirits?",
-            "ford spirits?",
+            "ford? spirits?",
             "footsteps",
             "porch spirits",
         ],
-    ],
-    ["E. E. E.", ["sun ?strike", "cataclysm", "send straight", "sunstroke", "some strike"]],
-]
+    ]
+];
+
+export default invokerSpells
+    .map(([activeOrbs, regex]) => [activeOrbs.join(" "), regex]) 
     .map(
         ([activeOrbs, regex]) =>
             new Rule({
